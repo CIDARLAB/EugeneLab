5877e35d4f630f34d304d3fe44fbc4556f6b3f06
Ernst Oberortner <e.oberortner@gmail.com>
2014-12-24 16:51:15 -0500

    added for buildbot

---
+++ src/main/java/org/cidarlab/eugenelab/servlet/EugeneLabConstants.java
@@ -0,0 +1,14 @@
+package org.cidarlab.eugenelab.servlet;
+
+/**
+ * The EugeneLabConstants class holds static members 
+ * that represent constants for the EugeneLabServlet
+ * 
+ * @author Ernst Oberortner
+ *
+ */
+public class EugeneLabConstants {
+
+	public static final String AUTHENTICATED = "authenticated";
+	public static final String APPLICATION_NAME = "eugenelab";
+}
